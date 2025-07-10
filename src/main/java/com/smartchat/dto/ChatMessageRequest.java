060b527c19757fc029b292d4f79c1d0fcdcf4258
Pedro Neto <pedro.neto.vertigo@cnpseguradora.com.br>
2025-07-10 17:33:51 -0300

    Including lombok in DTOs

--- src/main/java/com/smartchat/dto/ChatMessageRequest.java
+++ src/main/java/com/smartchat/dto/ChatMessageRequest.java
@@ -1,30 +1,13 @@
 package com.smartchat.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ChatMessageRequest {
     private String sender;
     private String message;
-
-    public ChatMessageRequest() {
-    }
-
-    public ChatMessageRequest(String sender, String message) {
-        this.sender = sender;
-        this.message = message;
-    }
-
-    public String getSender() {
-        return sender;
-    }
-
-    public void setSender(String sender) {
-        this.sender = sender;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
--- src/main/java/com/smartchat/dto/ChatMessageResponse.java
+++ src/main/java/com/smartchat/dto/ChatMessageResponse.java
@@ -1,30 +1,14 @@
 package com.smartchat.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 import java.time.LocalDateTime;
 
+@Data
+@AllArgsConstructor
 public class ChatMessageResponse {
     private String sender;
     private String content;
     private LocalDateTime timestamp;
-
-    public ChatMessageResponse() {
-    }
-
-    public ChatMessageResponse(String sender, String content, LocalDateTime timestamp) {
-        this.sender = sender;
-        this.content = content;
-        this.timestamp = timestamp;
-    }
-
-    public String getSender() {
-        return sender;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public LocalDateTime getTimestamp() {
-        return timestamp;
-    }
 }
